7ffceb59ff308b8336acd443c7112bf37625d077
DESKTOP-KGNJ6N7\Terry Jin <kukukthang@gmail.com>
2020-10-20 04:07:02 +0900

    first commit

---
+++ reduxapp/src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import TopHeader from "../components/TopHeader";
+import Footer from "../components/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <TopHeader />
+      메인 페이지 컴포넌트 내용
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
